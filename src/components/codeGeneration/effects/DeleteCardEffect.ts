7ae0da3b31d932e95981d105bf11cd6a4f7dba10
Jayden Holdsworth <70723772+Jayd-H@users.noreply.github.com>
2025-07-01 23:14:42 +0100

    feat: overhaul the destroy triggered card effect

--- src/components/codeGeneration/effects/DeleteCardEffect.ts
+++ src/components/codeGeneration/effects/DeleteCardEffect.ts
@@ -5,11 +5,7 @@ export const generateDeleteCardReturn = (effect?: Effect): EffectReturn => {
   const customMessage = effect?.customMessage;
 
   return {
-    statement: `__PRE_RETURN_CODE__
-                if not contains(card.ability.extra.destructo, context.other_card) then
-                    card.ability.extra.destructo[#card.ability.extra.destructo + 1] = context.other_card
-                end
-                __PRE_RETURN_CODE_END__`,
+    statement: "",
     message: customMessage ? `"${customMessage}"` : `"Destroyed!"`,
     colour: "G.C.RED",
   };
