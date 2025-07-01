d2c16cfc00dfe5b456129c17659044dfbcbd202c
Jayden Holdsworth <70723772+Jayd-H@users.noreply.github.com>
2025-07-01 22:39:42 +0100

    fix: destroy card effect incorrectly destroying cards
    this is still not a great solution

--- src/components/codeGeneration/effects/DeleteCardEffect.ts
+++ src/components/codeGeneration/effects/DeleteCardEffect.ts
@@ -5,10 +5,11 @@ export const generateDeleteCardReturn = (effect?: Effect): EffectReturn => {
   const customMessage = effect?.customMessage;
 
   return {
-    statement: `func = function()
-                    context.other_card:start_dissolve()
-                    return true
-                end`,
+    statement: `__PRE_RETURN_CODE__
+                if not contains(card.ability.extra.destructo, context.other_card) then
+                    card.ability.extra.destructo[#card.ability.extra.destructo + 1] = context.other_card
+                end
+                __PRE_RETURN_CODE_END__`,
     message: customMessage ? `"${customMessage}"` : `"Destroyed!"`,
     colour: "G.C.RED",
   };
