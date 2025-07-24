c740dda68abfe9e6a1ac2022ae97eec597307872
ItsRedCropie <franek.banas1@gmail.com>
2025-07-24 02:51:07 +0200

    fix: proper #216 (test your code kids)

--- src/components/codeGeneration/Jokers/index.ts
+++ src/components/codeGeneration/Jokers/index.ts
@@ -281,7 +281,9 @@ const generateSingleJokerCode = (
     passiveEffects,
     calculateResult?.configVariables || []
   );
-  jokerCode += `,\n\n    ${locVarsCode}`;
+  if (locVarsCode) {
+    jokerCode += `,\n\n    ${locVarsCode}`;
+  }
 
   const setStickerCode = generateSetAbilityFunction(joker);
   if (setStickerCode) {
@@ -1314,10 +1316,10 @@ const generateLocVarsFunction = (
   joker: JokerData,
   passiveEffects: PassiveEffectResult[],
   collectedConfigVariables: ConfigExtraVariable[]
-): string => {
+): string | null => {
   const descriptionHasVariables = joker.description.includes("#");
   if (!descriptionHasVariables) {
-    return ``;
+    return null;
   }
 
   const variablePlaceholders = joker.description.match(/#(\d+)#/g) || [];
@@ -1329,7 +1331,7 @@ const generateLocVarsFunction = (
   );
 
   if (maxVariableIndex === 0) {
-    return ``;
+    return null;
   }
 
   const allVariables = getAllVariables(joker);
