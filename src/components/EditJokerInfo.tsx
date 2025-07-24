dc82fddbb8da8c32659d0253d337b9f08974a5d9
fr0derick <32341462+fr0derick@users.noreply.github.com>
2025-07-24 11:19:05 +0100

    undo modal changes
    whoops

--- src/components/EditJokerInfo.tsx
+++ src/components/EditJokerInfo.tsx
@@ -671,7 +671,7 @@ const EditJokerInfo: React.FC<EditJokerInfoProps> = ({
       <div className="flex items-start gap-8 max-h-[90vh]">
         <div
           ref={modalRef}
-          className="bg-black-dark border-2 border-black-lighter rounded-lg w-full md:w-[90vw] lg:w-[100vh] h-[90vh] flex flex-col relative overflow-hidden"
+          className="bg-black-dark border-2 border-black-lighter rounded-lg w-[100vh] h-[90vh] flex flex-col relative overflow-hidden"
         >
           <div className="flex ">
             {tabs.map((tab, index) => {
