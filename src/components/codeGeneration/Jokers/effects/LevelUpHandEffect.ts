9b3fd06f8422756b7d9f6072979fae1fb1f55d30
Kevin Brasilleiro <kevin.brasileiro08@gmail.com>
2025-07-29 21:22:38 -0300

    fix: allow multiple levelUpHand effects in a row

--- src/components/codeGeneration/Jokers/effects/LevelUpHandEffect.ts
+++ src/components/codeGeneration/Jokers/effects/LevelUpHandEffect.ts
@@ -47,107 +47,99 @@ export const generateLevelUpHandReturn = (
     valueCode = "card.ability.extra.levels";
   }
 
+  const targetHandVar = sameTypeCount === 0 ? `target_hand` : `target_hand${sameTypeCount + 1}`
+
   const handSelection = (effect?.params?.hand_selection as string) || "current";
   const specificHand = (effect?.params?.specific_hand as string) || "High Card";
-
+  
   let handDeterminationCode = "";
-  if (handSelection === "specific") {
-    handDeterminationCode = `local target_hand = "${specificHand}"`;
-  } else if (handSelection === "random") {
-    handDeterminationCode = `local available_hands = {}
-                for k, v in pairs(G.GAME.hands) do
-                    if v.visible and v.level >= 1 then
-                        table.insert(available_hands, k)
-                    end
-                end
-                local target_hand = #available_hands > 0 and pseudorandom_element(available_hands, pseudoseed('level_up_hand')) or "High Card"`;
-  } else if (handSelection === "most") {
-    handDeterminationCode = `local temp_played = 0
-                            local temp_order = math.huge
-                            local target_hand
-                            for hand, value in pairs(G.GAME.hands) do
-                              if value.played > temp_played and value.visible then
-                                temp_played = value.played
-                                temp_order = value.order
-                                target_hand = hand
-                              else if value.played == temp_played and value.visible then
-                                if value.order < temp_order then
-                                  temp_order = value.order
-                                  target_hand = hand
-                                end
-                              end 
-                              end
-                            end`;
-  } else if (handSelection === "least") {
-    handDeterminationCode = `local temp_played = math.huge
-                            local temp_order = math.huge
-                            local target_hand
-                            for hand, value in pairs(G.GAME.hands) do
-                              if value.played < temp_played and value.visible then
-                                temp_played = value.played
-                                temp_order = value.order
-                                target_hand = hand
-                              else if value.played == temp_played and value.visible then
-                                if value.order < temp_order then
-                                  temp_order = value.order
-                                  target_hand = hand
-                                end
-                              end 
-                              end
-                            end`;
-  } else {
-    if (triggerType === "hand_discarded") {
-      return {
-        statement: `__PRE_RETURN_CODE__
-                local text, poker_hands, text_disp, loc_disp_text = G.FUNCS.get_poker_hand_info(G.hand.highlighted)
-                local target_hand = text
-                __PRE_RETURN_CODE_END__level_up = ${valueCode},
-                level_up_hand = target_hand`,
-        message: customMessage
-          ? `"${customMessage}"`
-          : `localize('k_level_up_ex')`,
-        colour: "G.C.RED",
-        configVariables:
-          configVariables.length > 0 ? configVariables : undefined,
-      };
-    } else {
-      const handToLevelUp = triggerType === "hand_played" ? "context.scoring_name" : '"High Card"'
-      return {
-        statement: `level_up = ${valueCode},
-                level_up_hand = ${handToLevelUp}`,
-        message: customMessage
-          ? `"${customMessage}"`
-          : `localize('k_level_up_ex')`,
-        colour: "G.C.RED",
-        configVariables:
-          configVariables.length > 0 ? configVariables : undefined,
-      };
-    }
+  switch (handSelection) {
+    case ("specific"):
+      handDeterminationCode = `${targetHandVar} = "${specificHand}"`;
+      break
+    case ("random"):
+      handDeterminationCode = `
+        available_hands = {}
+        for hand, value in pairs(G.GAME.hands) do
+          if value.visible and value.level >= 1 then
+            table.insert(available_hands, hand)
+          end
+        end
+        ${targetHandVar} = #available_hands > 0 and pseudorandom_element(available_hands, pseudoseed('level_up_hand')) or "High Card"
+        `;
+      break
+    case ("most"):
+      handDeterminationCode = `
+        temp_played = 0
+        temp_order = math.huge
+        for hand, value in pairs(G.GAME.hands) do 
+          if value.played > temp_played and value.visible then
+            temp_played = value.played
+            temp_order = value.order
+            ${targetHandVar} = hand
+          else if value.played == temp_played and value.visible then
+            if value.order < temp_order then
+              temp_order = value.order
+              ${targetHandVar} = hand
+            end
+          end
+          end
+        end
+      `;
+      break
+    case ("least"):
+      handDeterminationCode = `
+        temp_played = math.huge
+        temp_order = math.huge
+        for hand, value in pairs(G.GAME.hands) do 
+          if value.played < temp_played and value.visible then
+            temp_played = value.played
+            temp_order = value.order
+            ${targetHandVar} = hand
+          else if value.played == temp_played and value.visible then
+            if value.order < temp_order then
+              temp_order = value.order
+              ${targetHandVar} = hand
+            end
+          end
+          end
+        end
+      `;
+      break
+    case ("current"):
+      if (triggerType === "hand_discarded") {
+        handDeterminationCode = `
+          text, poker_hands, text_disp, loc_disp_text = G.FUNCS.get_poker_hand_info(G.hand.highlighted)
+          ${targetHandVar} = text
+        `;
+      } else if (triggerType === "hand_played") {
+        handDeterminationCode = `${targetHandVar} = context.scoring_name`;
+      } else {
+        handDeterminationCode = `${targetHandVar} = "High Card"`
+      }
+      break
   }
+  
+  return {
+    statement: `
+      __PRE_RETURN_CODE__
+      ${handDeterminationCode}
+      __PRE_RETURN_CODE_END__
+      level_up = ${valueCode},
+      level_up_hand = ${targetHandVar}`,
+    message: customMessage ? `${customMessage}` : `localize('k_level_up_ex')`,
+    colour: "G.C.RED",
+    configVariables: configVariables.length > 0 ? configVariables : undefined
+  }
+}
 
-  if (triggerType === "hand_discarded") {
-    return {
-      statement: `__PRE_RETURN_CODE__
-                ${handDeterminationCode}
-                __PRE_RETURN_CODE_END__level_up = ${valueCode},
-                level_up_hand = target_hand`,
-      message: customMessage
-        ? `"${customMessage}"`
-        : `localize('k_level_up_ex')`,
-      colour: "G.C.RED",
-      configVariables: configVariables.length > 0 ? configVariables : undefined,
-    };
-  } else {
-    return {
-      statement: `__PRE_RETURN_CODE__
-                ${handDeterminationCode}
-                __PRE_RETURN_CODE_END__level_up = ${valueCode},
-                level_up_hand = target_hand`,
-      message: customMessage
-        ? `"${customMessage}"`
-        : `localize('k_level_up_ex')`,
-      colour: "G.C.RED",
-      configVariables: configVariables.length > 0 ? configVariables : undefined,
-    };
-  }
-};
+      // return {
+      //   statement: `level_up = ${valueCode},
+      //           level_up_hand = ${handToLevelUp}`,
+      //   message: customMessage
+      //     ? `"${customMessage}"`
+      //     : `localize('k_level_up_ex')`,
+      //   colour: "G.C.RED",
+      //   configVariables:
+      //     configVariables.length > 0 ? configVariables : undefined,
+      // };
