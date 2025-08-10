a97608173e05f2a3e78e149d28f2136b780ef4b8
Jayden Holdsworth <70723772+Jayd-H@users.noreply.github.com>
2025-08-10 03:32:37 +0100

    fix: cloudflare web analytics now should point to the right domain

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,15 @@ import App from "./App.tsx";
 import ErrorBoundary from "./components/generic/ErrorBoundary.tsx";
 import "./index.css";
 
+const script = document.createElement("script");
+script.defer = true;
+script.src = "https://static.cloudflareinsights.com/beacon.min.js";
+script.setAttribute(
+  "data-cf-beacon",
+  '{"token": "d7ccf8e68a924adeb024b8a831be15e3"}'
+);
+document.head.appendChild(script);
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ErrorBoundary>
