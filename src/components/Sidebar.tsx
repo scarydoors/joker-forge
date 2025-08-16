bdb963200512d17af09099c9e402b35e995f890b
Jayden Holdsworth <70723772+Jayd-H@users.noreply.github.com>
2025-08-16 21:45:47 +0100

    chore: update version number

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -50,7 +50,7 @@ const Sidebar: React.FC<SidebarProps> = ({
   const [hoveredItem, setHoveredItem] = useState<string | null>(null);
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
-  const version: string = "v0.5.2";
+  const version: string = "v0.5.3";
 
   const handleSectionClick = (section: string) => {
     if (section === "github") {
